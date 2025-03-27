e895588f12106b6a6841f45939810ccc2f78a783
Adam <adamspeer@gmail.com>
2025-03-27 11:22:36 -0400

    Initial commit: WordPress Guest Author plugin

---
+++ includes/jpc-guest-author-posts.php
@@ -0,0 +1,225 @@
+<?php
+function jpc_guest_author_posts()
+{
+	// set up labels
+	$labels = array(
+		'name' => 'JPC Guest Authors',
+		'singular_name' => 'JPC Guest Author',
+		'add_new' => 'Add New JPC Guest Author',
+		'add_new_item' => 'Add New JPC Guest Author',
+		'edit_item' => 'Edit JPC Guest Author',
+		'new_item' => 'New JPC Guest Author',
+		'all_items' => 'All JPC Guest Authors',
+		'view_item' => 'View JPC Guest Author',
+		'search_items' => 'Search JPC Guest Authors',
+		'not_found' => 'No JPC Guest Authors Found',
+		'not_found_in_trash' => 'No JPC Guest Authors found in Trash',
+		'parent_item_colon' => '',
+		'menu_name' => 'JPC Guest Authors',
+	);
+	//register post type
+	register_post_type(
+		'jpc_guest_author',
+		array(
+			'labels' => $labels,
+			'has_archive' => true,
+			'public' => true,
+			'menu_icon' => 'dashicons-businessman',
+			'supports' => array('title', 'editor', 'thumbnail'),
+			'taxonomies' => array('post_tag', 'category'),
+			'exclude_from_search' => false,
+			'capability_type' => 'post',
+			'rewrite' => array('slug' => 'authors'),
+		)
+	);
+}
+add_action('init', 'jpc_guest_author_posts');
+
+//hook into the init action and call create_topics_nonhierarchical_taxonomy when it fires
+add_action('init', 'create_topics_nonhierarchical_taxonomy', 0);
+
+function create_topics_nonhierarchical_taxonomy()
+{
+	// Labels part for the GUI
+	$labels = array(
+		'name' => _x('Authors', 'taxonomy general name'),
+		'singular_name' => _x('Author Tag', 'taxonomy singular name'),
+		'search_items' => __('Search Author Tags'),
+		'popular_items' => __('Popular Author  Tags'),
+		'all_items' => __('All Author  Tags'),
+		'parent_item' => null,
+		'parent_item_colon' => null,
+		'edit_item' => __('Edit Author Tag'),
+		'update_item' => __('Update Author Tag'),
+		'add_new_item' => __('Add New Author Tag'),
+		'new_item_name' => __('New Author Tag Name'),
+		'separate_items_with_commas' => __('Separate Author Tag with commas'),
+		'add_or_remove_items' => __('Add or remove Author Tag'),
+		'choose_from_most_used' => __('Choose from the most used Author Tag'),
+		'menu_name' => __('Authors Tags'),
+	);
+
+	// Now register the non-hierarchical taxonomy like tag
+	register_taxonomy('jpc_guest_author_tags', array('post', 'jpc_guest_author'), array(
+		'hierarchical' => false,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var' => true,
+		'rewrite' => array('slug' => 'authors'),
+	));
+}
+
+function save_author_tag($post_id, $post, $update)
+{
+	$posttype = 'jpc_guest_author';
+
+	if ($posttype != $post->post_type) {
+		return;
+	}
+
+	if (isset($_REQUEST['post_title'])) {
+		$authortag = $_POST['post_title'];
+		$authorslug = str_replace(' ', '-', strtolower($_POST['post_title']));
+		wp_insert_term($authortag, 'jpc_guest_author_tags', array('slug' => $authorslug));
+	}
+}
+add_action('save_post', 'save_author_tag', 10, 3);
+
+add_filter('the_author', 'guest_author_name');
+add_filter('get_the_author_display_name', 'guest_author_name');
+
+function guest_author_name($name)
+{
+	global $post;
+	$authors = wp_get_post_terms($post->ID, 'jpc_guest_author_tags');
+
+	$author = "";
+	if (count($authors) > 0) {
+		foreach ($authors as $a) {
+			$author .= '<a href="' . get_term_link($a) . '">' . $a->name . '</a> ';
+		}
+		// Remove trailing space
+		$author = rtrim($author);
+	}
+
+	if ($author > "")
+		$name = $author;
+
+	return $name;
+}
+
+add_filter('the_author_link', 'guest_author_link_filter');
+function guest_author_link_filter($content)
+{
+	// No need to print the content, just return it
+	return $content;
+}
+
+/**
+ * Add styling for author links and add bullet separator
+ * Only on single post/page views
+ */
+add_action('wp_head', 'jpc_author_style_single');
+function jpc_author_style_single()
+{
+	// Only apply on single post/page views
+	if (!is_single() && !is_page()) {
+		return;
+	}
+	?>
+	<style type="text/css">
+		/* Make author links blue on single post pages */
+		.td-post-header .td-post-author-name a,
+		.meta-info a:not(.td-post-small-box a) {
+			color: #4db2ec !important;
+		}
+
+		/* Keep original styling for source links */
+		.td-post-small-box a {
+			color: white !important;
+		}
+
+		/* Hide the default dash separator */
+		.td-post-header .td-post-author-name .td-author-line {
+			display: none !important;
+		}
+	</style>
+	<script>
+		document.addEventListener('DOMContentLoaded', function () {
+			// Only run on single post/page views
+			if (!document.body.classList.contains('single') &&
+				!document.body.classList.contains('page')) {
+				return;
+			}
+
+			// Find all meta-info containers
+			var metaContainers = document.querySelectorAll('.meta-info');
+
+			metaContainers.forEach(function (container) {
+				// Get all direct children
+				var children = Array.from(container.children);
+
+				// Find the author link and date elements
+				var authorElement = children.find(el => el.tagName === 'A');
+				var dateElement = children.find(el => el.classList.contains('td-post-date'));
+
+				// If both elements exist and no bullet separator between them
+				if (authorElement && dateElement) {
+					// Check if there's already text with a bullet between them
+					var insertAfter = authorElement;
+					var hasExistingBullet = false;
+
+					// Check nodes between author and date
+					var currentNode = authorElement.nextSibling;
+					while (currentNode && currentNode !== dateElement) {
+						if (currentNode.nodeType === Node.TEXT_NODE &&
+							currentNode.textContent.includes('•')) {
+							hasExistingBullet = true;
+							break;
+						}
+						currentNode = currentNode.nextSibling;
+					}
+
+					// If no bullet exists, add one
+					if (!hasExistingBullet) {
+						var bullet = document.createTextNode(' • ');
+						if (insertAfter.nextSibling) {
+							container.insertBefore(bullet, insertAfter.nextSibling);
+						} else {
+							container.insertBefore(bullet, dateElement);
+						}
+					}
+				}
+			});
+
+			// Also handle the standard theme format
+			var authorElements = document.querySelectorAll('.td-post-header .td-post-author-name');
+
+			authorElements.forEach(function (el) {
+				// Check if there's a date element next to it
+				if (el.nextElementSibling &&
+					el.nextElementSibling.classList.contains('td-post-date')) {
+
+					// If no bullet already exists
+					if (!el.innerHTML.includes('•')) {
+						// Remove any existing dash separator
+						var dashElement = el.querySelector('.td-author-line');
+						if (dashElement) {
+							dashElement.remove();
+						}
+
+						// Add bullet separator
+						var bullet = document.createElement('span');
+						bullet.innerHTML = ' • ';
+						el.insertAdjacentElement('afterend', bullet);
+					}
+				}
+			});
+		});
+	</script>
+	<?php
+}
+?>
+
---
+++ jpc-guest-author.php
@@ -0,0 +1,274 @@
+<?php
+/*
+Plugin Name: JPC Guest Author Plugin
+Plugin URI: http://www.jewishpolicycenter.org
+Description: Custom functionality for the Guest Author Post Type
+Version: 1.0
+Author: JPC
+Author URI: http://www.jewishpolicycenter.org
+License: GPL2
+*/
+
+require_once('includes/jpc-guest-author-posts.php');
+
+
+class JPC_Author_Widget extends WP_Widget
+{
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct()
+	{
+		$widget_ops = array(
+			'class_name' => 'jpc_author_widget',
+			'description' => 'JPC Author Widget',
+		);
+		parent::__construct('jpc_author_widget', 'JPC Author Widget', $widget_ops);
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget($args, $instance)
+	{
+
+		$authortags = explode(',', $instance['authortags']);
+		$tids = array();
+		$tnames = array();
+		foreach ($authortags as $t) {
+			$term = get_term_by('slug', $t, 'jpc_guest_author_tags');
+
+			if ($term) {
+				$tids[] = $term->term_id;
+				$tnames[] = $term->name;
+			}
+
+		}
+		?>
+		<div class="td_block_wrap td_block_9 td-pb-border-top">
+			<h4 class="block-title">
+				<?php if (($instance['title']) > "") {
+					echo "<span> " . $instance['title'] . "</span>";
+				} else { ?>
+					<span>Articles by: <?php
+					$c = 0;
+					foreach ($tnames as $t) {
+						if ($c > 0) {
+							echo ", ";
+						}
+						echo $t;
+						$c++;
+					} ?></span>
+				<?php } ?>
+			</h4>
+			<?php
+
+			$paged = (get_query_var('page')) ? get_query_var('page') : 1;
+			$args2 = array(
+				'post_type' => 'post',
+				'posts_per_page' => $instance['postcount'],
+				'orderby' => 'date',
+				'order' => 'DESC',
+				'paged' => $paged,
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'jpc_guest_author_tags',
+						'terms' => $tids,
+					),
+				)
+			);
+			$loop2 = new WP_Query($args2);
+			?>
+
+			<?php if ($loop2->have_posts()) { ?>
+				<div class="jpc_ajax_content">
+					<div class="ajax-response">
+						<div class="innerajax">
+							<?php
+
+							while ($loop2->have_posts()):
+								$loop2->the_post(); ?>
+
+
+								<div class="td_block_inner">
+									<div class="td-block-span12">
+										<div class="td_module_8 td_module_wrap">
+											<div class="item-details">
+												<h3 class="entry-title td-module-title"><a href="<?php the_permalink(); ?>" rel="bookmark"
+														title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+												<div class="meta-info">
+													<?php
+													$postid = get_the_ID();
+													$thisPostACF = getPostACF($postid);
+													if ($thisPostACF['jpc_book_by']) {
+														echo "<strong>Book by:</strong> " . $thisPostACF['jpc_book_by'] . " <br /> ";
+														echo "<strong>Reviewed by:</strong> " . jpc_author_display($postid);
+													} else {
+														echo jpc_author_display(get_the_ID());
+													} ?>
+													<?php if ($thisPostACF['jpc_custom_date_display']) {
+														echo " • " . $thisPostACF['jpc_custom_date_display'];
+													} else {
+														echo " • ";
+														the_date(false);
+													}
+													?>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							<?php endwhile; ?>
+
+						</div>
+					</div>
+					<div style="display:none">
+						<div class="widget_post_count"><?php echo $instance['postcount']; ?></div>
+						<div class="author_term_id"><?php echo $tids[0]; ?></div>
+						<div class="author_term_name"><?php echo $tnames[0]; ?></div>
+						<div class="action_name">theme_post_example3</div>
+						<div class="offsetmax"><?php echo $loop2->found_posts; ?></div>
+					</div>
+					<div class="td-next-prev-wrap"><a href="#" class="ajax-post-back"><i
+								class="td-icon-font td-icon-menu-left"></i></a><a href="#" class="ajax-post-next"><i
+								class="td-icon-font td-icon-menu-right"></i></a></div>
+
+
+				</div>
+			<?php } ?>
+
+
+
+		</div>
+		<?php
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form($instance)
+	{
+		$title = "";
+		$authortags = "";
+		$postcount = 3;
+		if (isset($instance['title'])) {
+			$title = esc_attr($instance['title']);
+		}
+
+		if (isset($instance['authortags'])) {
+			$authortags = esc_attr($instance['authortags']);
+		}
+
+		if (isset($instance['postcount'])) {
+			$postcount = esc_attr($instance['postcount']);
+		}
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+				name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('authortags'); ?>"><?php _e('Author Tags'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('authortags'); ?>"
+				name="<?php echo $this->get_field_name('authortags'); ?>" type="text" value="<?php echo $authortags; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('postcount'); ?>"><?php _e('Post Count'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('postcount'); ?>"
+				name="<?php echo $this->get_field_name('postcount'); ?>" type="number" value="<?php echo $postcount; ?>" />
+		</p>
+		<?php
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['authortags'] = strip_tags($new_instance['authortags']);
+		$instance['postcount'] = strip_tags($new_instance['postcount']);
+		return $instance;
+	}
+}
+
+
+add_action('widgets_init', function () {
+	register_widget('JPC_Author_Widget');
+});
+
+
+
+
+
+add_action('wp_ajax_theme_post_example3', 'theme_post_example_init3');
+add_action('wp_ajax_nopriv_theme_post_example3', 'theme_post_example_init3');
+function theme_post_example_init3()
+{ ?>
+
+	<?php
+	$args3 = array(
+		'post_type' => 'post',
+		'posts_per_page' => $_POST['widget_post_count'],
+		'offset' => $_POST['author_offset'],
+		'orderby' => 'date',
+		'order' => 'DESC',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'jpc_guest_author_tags',
+				'terms' => $_POST['author_term_id'],
+			),
+		)
+	);
+	$loop3 = new WP_Query($args3);
+	?>
+
+
+	<div class="archive-row">
+		<?php while ($loop3->have_posts()):
+			$loop3->the_post(); ?>
+			<div class="td_block_inner">
+				<div class="td-block-span12">
+					<div class="td_module_8 td_module_wrap">
+						<div class="item-details">
+							<h3 class="entry-title td-module-title"><a href="<?php the_permalink(); ?>" rel="bookmark"
+									title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+							<div class="meta-info">
+								<?php
+								$postid = get_the_ID();
+								$thisPostACF = getPostACF($postid);
+								if ($thisPostACF['jpc_book_by']) {
+									echo "<strong>Book by:</strong> " . $thisPostACF['jpc_book_by'] . " <br /> ";
+									echo "<strong>Reviewed by:</strong> " . jpc_author_display($postid);
+								} else {
+									echo jpc_author_display(get_the_ID());
+								} ?>
+								<?php if ($thisPostACF['jpc_custom_date_display']) {
+									echo " • " . $thisPostACF['jpc_custom_date_display'];
+								} else {
+									echo " • ";
+									the_date(false);
+								}
+								?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		<?php endwhile; ?>
+
+	<?php } ?>
+
